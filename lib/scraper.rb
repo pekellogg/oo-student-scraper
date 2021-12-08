b5bf02827f1a632b2bc3c3eb88428fe564f1d92a
Peyton <pekellogg@hotmail.com>
2021-12-08 14:29:43 -0800

    Completed .scrape_index_page

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,28 @@
-require 'open-uri'
-require 'pry'
+require "open-uri"
+require "pry"
+require "nokogiri"
 
 class Scraper
 
+  # :name, :location, :profile_url
   def self.scrape_index_page(index_url)
     
-  end
+    doc = Nokogiri::HTML(URI.open(index_url))
+    
+    profiles = []
 
+    doc.css("div.roster-cards-container").each do |student_card|
+      student_card.css(".student-card a").each do |student|
+        name = student.css(".student-name").text
+        location = student.css(".student-location").text
+        link = "#{student.attr("href")}"
+        profiles << {name: name, location: location, profile_url: link}
+      end
+    end
+    profiles
+  end
+  
+  #:twitter, :linkedin, :github, :blog, :profile_quote, :bio
   def self.scrape_profile_page(profile_url)
     
   end
